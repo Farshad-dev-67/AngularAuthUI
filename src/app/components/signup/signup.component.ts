e8364628854efff9a903028aa53451228d7ca3e9
Administrator <lari.farshad@gmail.com>
2023-06-16 21:13:08 +0330

    create login and sign up

---
+++ src/app/components/signup/signup.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.scss']
+})
+export class SignupComponent {
+  type: string = 'password';
+  eyeIcon: string = 'fa-eye-slash';
+  isText: boolean = false;
+  hideShow(){
+    this.isText = !this.isText;
+    this.isText ? this.eyeIcon = 'fa-eye' : this.eyeIcon = 'fa-eye-slash';
+    this.isText ? this.type = 'text' : this.type = 'password';
+  }
+}
